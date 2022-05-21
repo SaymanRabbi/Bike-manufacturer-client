369dba1837f0c196bdf62e1162bd63a29ee4d0f0
sayman <mdrabbi.asm@gmail.com>
2022-05-21 21:33:35 +0600

    add helment provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
 
 function App() {
   return (
+    <HelmetProvider>
     <div >
       <h1>Hello World</h1>
-    </div>
+      </div>
+      </HelmetProvider>
   );
 }
 
