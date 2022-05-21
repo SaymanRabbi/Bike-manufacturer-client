44660f21190c027b6479f8cda3b9aa0fddd65281
sayman <mdrabbi.asm@gmail.com>
2022-05-21 22:43:02 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Blog from './Pages/Blog/Blog';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <HelmetProvider>
     <div >
-        <h1>Hello World</h1>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/blog' element={<Blog></Blog>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
         <ToastContainer></ToastContainer>
       </div>
       </HelmetProvider>
