bf5d0dc3f3cb722862cd2295f125e71423f5021c
sayman <mdrabbi.asm@gmail.com>
2022-05-21 21:54:10 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <HelmetProvider>
     <div >
-      <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <ToastContainer></ToastContainer>
       </div>
       </HelmetProvider>
   );
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAaV5ERH2p1q9FvbaLjDgVQAkwoR-Zyzk",
+  authDomain: "assignment-12-client-dde4f.firebaseapp.com",
+  projectId: "assignment-12-client-dde4f",
+  storageBucket: "assignment-12-client-dde4f.appspot.com",
+  messagingSenderId: "672432473729",
+  appId: "1:672432473729:web:ef369156b7f0f8ffa7ef80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
